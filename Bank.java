08bda3ec8b25d3f28b373c4a6b92e8d527ee77d8
chungeuna <54252671+chungeuna@users.noreply.github.com>
2019-09-06 17:17:15 +0900

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+
 class Account {
 	public Account(String accountNo, String name) {
 	}
 	private String accountNo;
 	private String name;
+	
 	public String getAccountNo() {
 		return accountNo;
 	}
@@ -18,15 +20,15 @@ class Account {
 	}
 	
 }
-public class Bank {
+public class Bank{
 	private ArrayList<Account> accounts = new ArrayList<Account>();
 	private int totalAccount;
 	
 	
-	void addAccount(String accountNo, String name) {
+	void addAccount(String accountNo, String name) { //계좌를 생성한다 
 		accounts.add(new Account(accountNo,name));		
 	}
-	Account getAccount(String accountNo) {
+	Account getAccount(String accountNo) { //계좌번호로 계좌를 찾는다 
 		for(Account account : accounts) {
 			if(account.getAccountNo() == accountNo) {
 				System.out.println(getAccounts());
@@ -34,22 +36,32 @@ public class Bank {
 		}
 		return null; //수정해야한다 
 	}
-	Account[] findAccounts(String name) { //함수안에 제너릭 하나 더 만들고 그 주소값을 리턴 
-		ArrayList<Account[]> list = new ArrayList<Account[]>(); // 일단 account[]를 넣어보았다... 
-;		for(Account account : accounts) {
+	Account[] findAccounts(String name) { //함수안에 제너릭 하나 더 만들고 그 주소값을 리턴   //소유자명으로 계좌를 찾는다 
+		ArrayList<Account> list = new ArrayList<Account>(); // 일단 account[]를 넣어보았다... 
+;		/*for(Account account : accounts) {
 			if(account.getName() == name) {
-				list.add(account.getAccountNo());
-			}	
+				
+				
+		}*/
+		for(int i = 0; i < accounts.size() ; i++) {
+			if(accounts.get(i).getName() == name) {
+				//list.add(getAccounts());
+				list.add(accounts.get(i));
+				list.get(i).getAccountNo();
+				//list.add(i); 
+				return null;
+			}
 		}
 		return ; //수정 무엇을 리턴할까 		
 	}
-	Account[] getAccounts() {
+	Account[] getAccounts() { //계좌 목록을 본다 
 		for(Account account : accounts) {
 			System.out.println(account);
 		}
+		return null;
 	}
-	int getTotalAccount() {
-		
+	int getTotalAccount() { //총계좌수를 반환한다 
+		return accounts.size();
 	}
 	
 }
