b769bdcb6739a5955c7504c47342caf055b6965f
4transparency <53933566+4transparency@users.noreply.github.com>
2019-09-06 19:07:49 +0900

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -1,25 +1,7 @@
+package kr.or.bit;
+
 import java.util.ArrayList;
 
-class Account {
-	public Account(String accountNo, String name) {
-	}
-	private String accountNo;
-	private String name;
-	
-	public String getAccountNo() {
-		return accountNo;
-	}
-	public void setAccountNo(String accountNo) {
-		this.accountNo = accountNo;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-}
 public class Bank{
 	private ArrayList<Account> accounts = new ArrayList<Account>();
 	private int totalAccount;
@@ -28,17 +10,25 @@ public class Bank{
 	void addAccount(String accountNo, String name) { //계좌를 생성한다 
 		accounts.add(new Account(accountNo,name));		
 	}
-	Account getAccount(String accountNo) { //계좌번호로 계좌를 찾는다 
-		for(Account account : accounts) {
-			if(account.getAccountNo().equals(accountNo)) {
-				System.out.println(getAccounts());
+	ArrayList<Account> getAccount(String accountNo) {
+		int index2 = 0;//계좌번호로 계좌를 찾는다 
+		ArrayList<Account> acc = new ArrayList<Account>();
+		
+		for(int i=0; i<accounts.size(); i++) {
+			if(accounts.get(i).getAccountNo().equals(accountNo)) {
+				//System.out.println(getAccounts());
+				acc.add(accounts.get(i));
+				System.out.println("dfjktttttt");
 			}
 		}
-		return null; //수정해야한다 
+		
+			
+		return acc; //수정해야한다 
 	}
+	
 	ArrayList<Account> findAccounts(String name) { //함수안에 제너릭 하나 더 만들고 그 주소값을 리턴   //소유자명으로 계좌를 찾는다 
 		ArrayList<Account> list = new ArrayList<Account>(); // 일단 account[]를 넣어보았다... 
-;		/*for(Account account : accounts) {
+		/*for(Account account : accounts) {
 			if(account.getName() == name) {
 				
 				
@@ -55,13 +45,21 @@ public class Bank{
 		return null; //수정 무엇을 리턴할까 		
 	}
 	Account getAccounts() { //계좌 목록을 본다 
-		for(Account account : accounts) {
-			System.out.println(account);
-		}
-		return null;
+		
+			return accounts.get(0);
+		
+		
 	}
+	
 	int getTotalAccount() { //총계좌수를 반환한다 
 		return accounts.size();
 	}
+	@Override
+	public String toString() {
+		return "Bank [accounts=" + accounts + ", totalAccount=" + totalAccount + "]";
+	}
+	
+	
+	
 	
 }
